3adc1532a88814a5570aefc4ed5af5794fcbdeea
JanhviSingh2302 <janhvisingh232007@gmail.com>
2026-06-06 20:57:45 +0530

    Day 9

---
+++ Q35.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,i,j;
+    cout<<"Enter the value of n";
+    cin>>n;
+    char ch='A';
+     for(i=1;i<=n;i++)
+     {
+        
+        for(j=1;j<=i;j++)
+        {
+            cout<<ch;
+        
+        }
+        ch++;
+        cout<<endl;
+     }
+     return 0;
+}
